a786dc72a93fe2ecaddb6f468172dd7bf823d23e
Sven Pfotzer <ucera@student.kit.edu>
2020-04-06 23:54:31 +0200

    changed start script

--- start.py
+++ start.py
@@ -1,21 +1,18 @@
 # Run with Python 3.7
 
 import sys
-from pathlib import Path
 import sudoku_ar.app as app
 
-
-PROJECT_ROOT = Path(__file__).parent.absolute()
-
 if len(sys.argv) == 1:
-    app.run("https://test:test123@192.168.178.70:8080/video")
+    print("Options are --train, --gen, --path")
 elif len(sys.argv) == 2:
-    if str(sys.argv[1]) == "-ph":
-        # cv2.imread does not support pathlib paths
-        app.run(str(PROJECT_ROOT / "sudoku_ar/resources/photo.jpg"))
-    elif str(sys.argv[1]) == "-train":
+    if str(sys.argv[1]) == "--train":
         import sudoku_ar.classifier.number_classifier as classifier
         classifier.train()
-    elif str(sys.argv[1]) == "-gen":
+    elif str(sys.argv[1]) == "--gen":
         import sudoku_ar.data.train_data_gen_script as generator
         generator.main()
+elif len(sys.argv) == 3:
+    if str(sys.argv[1]) == "--path":
+        # cv2.imread does not support pathlib paths
+        app.run(str(sys.argv[2]))
